87627a39957d108705b8d6ad864997091400b3c6
InGyu Moon <ansdlsrb1121@naver.com>
2024-05-13 11:21:04 +0900

    feat: 회원정보 수정, 삭제 추가

--- src/app/components/UserInput.tsx
+++ src/app/components/UserInput.tsx
@@ -2,9 +2,12 @@
 import React from 'react';
 import {useState,useEffect} from 'react'
 import {user} from '../types/user'
+import UserUpdateForm from './UserUpdateForm';
 
 
 export default function UserInput(){
+
+    const [updateFlags,setUpdateFlags] = useState<boolean[]>([]);
 
     const [userInputData, setUserData] = useState({
         userEmail: '',
@@ -63,7 +66,39 @@ export default function UserInput(){
         }
     }
 
+    async function deleteUser(userEmail:string){
+        console.log(userEmail);
+        try{
+            const response = await fetch('http://localhost:3000/user',{
+                method:"DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    userEmail: userEmail
+                })
+            });
+            if(response.ok){
+                console.log("회원삭제 성공");
+                getAllUsers();
+                /**
+                 * 다시 랜더링 할때 
+                 */
+            }else{
+                throw new Error('Network response was not ok.');
+            }
+        }catch(error){
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    }
 
+    function toggleUpdateFlag(index: number) {
+        const newFlags = [...updateFlags];
+        newFlags[index] = !newFlags[index];
+        setUpdateFlags(newFlags);
+        getAllUsers();
+    }
+    
     return(
         <div>
             <h3>user 추가</h3>
@@ -83,15 +118,34 @@ export default function UserInput(){
 
             <h3>모든 사용자 정보</h3>
             <hr></hr>
-            {allUsers.map(user => (
-                    <div key={user._id}>
-                            Email: {user.userEmail}<br></br>
-                            NickName: {user.nickname}<br></br>
-                            JoinDate: {user.joinDate}<br></br>
-                            <hr></hr>
+            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {allUsers.map((user,index) => (
+                    <div key={user._id} style={{ flex: '0 0 calc(25% - 20px)', marginBottom: '20px', marginRight: '20px' }}>
+                        <div style={{ display: 'flex', marginBottom: '10px' }}>
+                            { updateFlags[index] ? 
+                            <div>
+                                <UserUpdateForm user={{userEmail:user.userEmail,nickname:user.nickname}} updateSuccess={()=>toggleUpdateFlag(index)}/>
+                            </div> :
+                            <>
+                            <div>
+                                <p><b>Email:</b> {user.userEmail}<br /></p>
+                                <p><b>NickName:</b> {user.nickname}<br /></p>
+                                <p><b>JoinDate:</b> {user.joinDate}<br /></p>
+                            </div>
+                            <div style={{ display: 'flex', alignItems: 'center', marginLeft: '10px' }}>
+                                <button onClick={() => toggleUpdateFlag(index)}>수정</button>
+                                <button onClick={()=>deleteUser(user.userEmail) }style={{ marginLeft: '30px' }}>삭제</button>
+                            </div>
+                            </>
+                            }
+                            
+                            
+                    </div>
+                    <hr/>
                     </div>
                 ))}
-
+            </div>
         </div>
     );
 }
+// onClick={()=>updateUser(user.userEmail,user.nickname) }
---
+++ src/app/components/UserUpdateForm.tsx
@@ -0,0 +1,68 @@
+'use client'
+import React from 'react';
+import {useState} from 'react'
+import { user } from "../types/user";
+
+interface props{
+    user:user;
+    updateSuccess:any;
+}
+
+// user:user
+// user:props
+
+export default function UserUpdateForm({user,updateSuccess}:props){
+
+    const [updateData,setUpdateData] = useState({
+        password: '',
+        nickname: user.nickname
+    });
+
+    function dataChange(e:any){
+        const { name, value } = e.target;
+        setUpdateData({
+            ...updateData,
+            [name]: value
+        })
+    }
+
+    async function handleSubmit(event:any){
+        event.preventDefault();
+        try{
+            const response = await fetch('http://localhost:3000/user',{
+                method:"PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    userEmail: user.userEmail,
+                    nickname: updateData.nickname,
+                    password: updateData.password
+                })
+            });
+            if(response.ok){
+                console.log("회원수정 성공");
+                updateSuccess();
+            }else{
+                throw new Error('Network response was not ok.');
+            }
+        }catch(error){
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+                <label htmlFor="userEmail">Email: </label>
+                <input type="text" name="userEmail" value={user.userEmail} readOnly />
+                <br/>
+                <label htmlFor="password">비밀번호: </label>
+                <input type="password" name="password" onChange={dataChange} required />
+                <br/>
+                <label htmlFor="nickname">사용자명: </label>
+                <input type="text" name="nickname" defaultValue={user.nickname} onChange={dataChange} required />
+                <br/>
+                <button type='submit'>수정</button>
+        </form>
+    )
+}
